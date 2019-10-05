96a6a371de85c176772fb8483f87f7402e4b6ea8
Maria Tudose <maria.tudose@aalto.fi>
2019-10-05 23:20:26 +0300

    implemented event listing for event finder page

---
+++ src/components/EventFinder.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+    <div class="ui left aligned container">
+      <div class="ui list" v-for="event in events" v-bind:key="event.id">
+        <div class="item">
+          <img class="ui tiny rounded right floated image" v-bind:src="event.image" />
+            <div class="ui header">{{ event.name }}</div>
+            <div>{{ event.location }}</div>
+            <div>{{ event.organizer }}</div>
+            <div>{{ event.date }}</div>
+        </div>
+        <div class="ui divider"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { events } from "./dummyEvents.js";
+
+export default {
+  name: "EventFinder",
+  data: function() {
+    return {
+      events
+    };
+  }
+};
+</script>
+
+<style scoped>
+.wrapper {
+  padding: 20px 0 60px 0;
+}
+
+.ui.image {
+  height: 75px;
+  background-size: contain;
+}
+
+.ui.divider {
+  margin: 0.5em 0;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import Home from './components/Home.vue';
 import Login from './components/Login.vue';
+import EventFinder from './components/EventFinder.vue'
 
 const routes = [
     { path: '/', component: Home },
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/events', component: EventFinder }
 ]
 
 export default routes;
