a85e8445830c2590d5ae82c8d25b33442a4a0bf5
lari alakukku <lari.alakukku@aalto.fi>
2019-10-06 14:08:29 +0300

    Moved the navbar to its own component file

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,22 @@
 <template>
   <div id="app">
-    <!-- route outlet -->
-    <!-- component matched by the route will render here -->
-    <router-view />
-    <nav class='ui  inverted bottom fixed labeled three item icon menu'>
-      <!-- use router-link component for navigation. -->
-      <!-- specify the link by passing the `to` prop. -->
-      <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
-      <router-link class='item' to="/">
-        <i class="compass icon"></i>
-        Events
-      </router-link>
-      <router-link class='item' to="/">
-        <i class="home icon"></i>
-        Home
-      </router-link>
-      <router-link class='item' to="/">
-        <i class="users icon"></i>
-        Groups
-      </router-link>
-    </nav>
+    
+    <Navbar />
   </div>
 </template>
 
 <script src="https://unpkg.com/vue/dist/vue.js"></script>
 <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
-<script>
 
-export default {
-  name: "app",
-  components: {
-    
-  }
-};
+<script>
+  import Navbar from "./components/Navbar.vue";
+
+  export default {
+    name: "app",
+    components: {
+      Navbar
+    }
+  };
 </script>
 
 <style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="navbar">
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <router-view />
+    <nav class='ui  inverted bottom fixed labeled three item icon menu'>
+      <!-- use router-link component for navigation. -->
+      <!-- specify the link by passing the `to` prop. -->
+      <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
+      <router-link class='item' to="/">
+        <i class="compass icon"></i>
+        Events
+      </router-link>
+      <router-link class='item' to="/">
+        <i class="home icon"></i>
+        Home
+      </router-link>
+      <router-link class='item' to="/">
+        <i class="users icon"></i>
+        Groups
+      </router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Navbar"
+  };
+</script>
+
+<style scoped>
+</style>
