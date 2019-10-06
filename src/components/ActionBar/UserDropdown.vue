8751b9f54597219910f91e534ea55fec65b3f15f
lari alakukku <lari.alakukku@aalto.fi>
2019-10-06 15:31:43 +0300

    Added log in button

--- src/components/ActionBar/UserDropdown.vue
+++ src/components/ActionBar/UserDropdown.vue
@@ -1,29 +1,58 @@
 <template>
-  <sui-dropdown
-    class="icon"
-    icon="empty"
-    id="user-dropdown"
+  <div id="user-dropdown">
+  <sui-button
+    v-if="!loggedIn"
+    v-on:click="handleClick"
+    basic
+    positive
   >
+    Log in
+  </sui-button>
+
+    <sui-dropdown
+      v-if="loggedIn"
+      class="icon"
+      icon="empty"
+      id="user-dropdown"
+    >
     <sui-icon name="ellipsis vertical" size="large" />
-    <sui-dropdown-menu>
-      <sui-dropdown-item>
-        <sui-icon name="settings" />
-          Settings
-      </sui-dropdown-item>
+      <sui-dropdown-menu>
+        <sui-dropdown-item>
+          <sui-icon name="settings" />
+            Settings
+        </sui-dropdown-item>
 
-      <sui-divider horizontal />
+        <sui-divider horizontal />
 
-      <sui-button icon="log out" negative>
-        Log out
-      </sui-button>
-    </sui-dropdown-menu>
+        <sui-button
+          icon="log out"
+          negative
+          v-on:click="handleClick"
+        >
+          Log out
+        </sui-button>
+      </sui-dropdown-menu>
 
-  </sui-dropdown>
+    </sui-dropdown>
+  </div>
 </template>
 
 <script>
+  function handleClick() {
+    this.loggedIn = !this.loggedIn
+  }
+
   export default {
     name: "UserDropdown",
+    data: function () {
+      return {
+        loggedIn: false
+      }
+    },
+    methods: {
+      handleClick
+    }
+    
   }
 </script>
 
