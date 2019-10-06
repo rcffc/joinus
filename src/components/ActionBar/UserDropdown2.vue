8942c1f0aa1cd701719f425363b69e83ac2277e7
lari alakukku <lari.alakukku@aalto.fi>
2019-10-06 15:36:16 +0300

    Added alternative versions of dropdown

---
+++ src/components/ActionBar/UserDropdown2.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="user-dropdown">
+  <sui-button
+    v-if="!loggedIn"
+    v-on:click="handleClick"
+    basic
+    positive
+  >
+    Log in
+  </sui-button>
+
+    <sui-dropdown
+      v-if="loggedIn"
+      class="icon primary basic large circular"
+      icon="user"
+      button
+    >
+      <sui-dropdown-menu>
+        <sui-dropdown-item>
+          <sui-icon name="settings" />
+            Settings
+        </sui-dropdown-item>
+
+        <sui-divider horizontal />
+
+        <sui-button
+          icon="log out"
+          fluid
+          negative
+          v-on:click="handleClick"
+        >
+          Log out
+        </sui-button>
+      </sui-dropdown-menu>
+
+    </sui-dropdown>
+  </div>
+</template>
+
+<script>
+  function handleClick() {
+    this.loggedIn = !this.loggedIn
+  }
+
+  export default {
+    name: "UserDropdown",
+    data: function () {
+      return {
+        loggedIn: false
+      }
+    },
+    methods: {
+      handleClick
+    }
+    
+  }
+</script>
+
+<style scoped>
+</style>
