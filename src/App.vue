01c78c6d4150750e3de6415039f2257dddfd82ae
lari alakukku <lari.alakukku@aalto.fi>
2019-10-06 14:00:44 +0300

    Removed HelloWorld

--- src/App.vue
+++ src/App.vue
@@ -26,12 +26,11 @@
 <script src="https://unpkg.com/vue/dist/vue.js"></script>
 <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    
   }
 };
 </script>
