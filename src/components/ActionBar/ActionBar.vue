857f0edc2ea5b76a6bc4475cb4c71ce146574084
Taro <jollypumpkin@mailbox.org>
2019-10-07 11:44:50 +0300

    Made Login Button to a Ghost Button & Embedded it into a Top Menu. Added a right menu search bar.

--- src/components/ActionBar/ActionBar.vue
+++ src/components/ActionBar/ActionBar.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="ui segment inverted left aligned" id="action-bar">
-    <user-dropdown/>
+  <div class="ui segment inverted" id="action-bar">
+    <div class="ui top attached menu inverted">
+      <div class="left aligned">
+        <user-dropdown/>
+      </div>
+      <div class="right menu">
+        <search-bar/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import UserDropdown from "./UserDropdown.vue";
+  import SearchBar from "./SearchBar.vue"
 
   export default {
     name: "ActionBar",
     components: {
-      UserDropdown
+      UserDropdown,
+      SearchBar
     }
   }
 </script>
---
+++ src/components/ActionBar/SearchBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="search-bar">
+    <div class="ui search">
+      <div class="ui icon input">
+        <input class="prompt" type="text" placeholder="Look for events..." />
+        <i class="search icon"></i>
+      </div>
+      <div class="results"></div>
+    </div>
+  </div>
+</template>
