5ee2f1b9122c5283bb45dcfc5c99e659fd30bd7d
lari alakukku <lari.alakukku@aalto.fi>
2019-10-07 13:04:38 +0300

    Added routing to event page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
-import Home from './components/Home.vue';
-import Login from './components/Login.vue';
+import Home from './components/Home.vue'
+import Login from './components/Login.vue'
 import EventFinder from './components/EventFinder.vue'
+import Event from './components/Event.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/login', component: Login },
-    { path: '/events', component: EventFinder }
+    { path: '/events', component: EventFinder },
+    { path: '/events/:id',
+        component: Event,
+        props: { default: true }
+    }
 ]
 
 export default routes;
