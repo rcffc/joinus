9ef835276fb06c9dbbb8991ab491e24389a3b247
lari alakukku <lari.alakukku@aalto.fi>
2019-10-07 16:44:56 +0300

    Added filter button

--- src/components/ActionBar/SearchBar.vue
+++ src/components/ActionBar/SearchBar.vue
@@ -2,10 +2,27 @@
   <div class="search-bar">
     <div class="ui search">
       <div class="ui icon input">
-        <input class="prompt" type="text" placeholder="Look for events..." />
+        <button id="filter-button" class="ui button icon white">
+          <i class="filter icon"></i>
+        </button>
+        <input id="search" class="prompt" type="text" placeholder="Look for events..." />
         <i class="search icon"></i>
       </div>
       <div class="results"></div>
     </div>
   </div>
 </template>
+
+<style scoped>
+  #search {
+    border-bottom-left-radius: 0 !important;
+    border-top-left-radius: 0 !important;  
+  }
+
+  #filter-button {
+    border-radius: 500rem;
+    border-bottom-right-radius: 0 !important;
+    border-top-right-radius: 0 !important;
+    margin-right: 0%;
+  }
+</style>
