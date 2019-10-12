568cb83122f4a306d0263a94bfe0631ecf33cb6e
lari alakukku <lari.alakukku@aalto.fi>
2019-10-12 17:56:09 +0300

    Fixed old references to semantic-ui-vue

--- src/components/ActionBar/ActionBar.vue
+++ src/components/ActionBar/ActionBar.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="ui segment inverted" id="action-bar">
-    <sui-segment id="action-bar" inverted v-if="this.$route.path === '/login'">
+    <div class="ui segment inverted" id="action-bar" v-if="this.$route.path === '/login'">
       <img class="logo" src="../../../assets/join-us-white.png" />
-    </sui-segment>
-    <sui-segment id="action-bar" inverted v-else>
-    <div class="ui top attached menu inverted">
-      <div class="left aligned">
-        <user-dropdown/>
-      </div>
-      <div class="right menu">
-        <search-bar/>
+    </div>
+
+    <div class="ui segment inverted" id="action-bar" v-else>
+      <div class="ui top attached menu inverted">
+        <div class="left aligned">
+          <user-dropdown/>
+        </div>
+        <div class="right menu">
+          <search-bar/>
+        </div>
       </div>
     </div>
-    </sui-segment>
   </div>
 </template>
 
