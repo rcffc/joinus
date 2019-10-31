716bfa6c0d573ed92c4cf73b678be414b9496bd5
lari alakukku <lari.alakukku@aalto.fi>
2019-10-31 11:08:02 +0200

    Changed calendar options menu to a modal

--- src/components/utils/CalendarButton.vue
+++ src/components/utils/CalendarButton.vue
@@ -1,19 +1,33 @@
 <template>
-  <div
-    :class="`ui dropdown ${ (misc) ? 'basic' : 'right labeled' } icon button ${ size } positive icon-button`" 
-  > 
-    <i class="calendar icon" />
-    {{ (misc) ? '' : 'Save the date' }}
+  <div>
+    <div
+      @click="clickHandler"
+      :class="`ui ${ (misc) ? 'basic' : 'right labeled' } icon button ${ size } positive icon-button`"
+    > 
+      <i class="calendar icon" />
+      {{ (misc) ? '' : 'Save the date' }}
+    </div>
 
-    <!-- calendar options -->
-    <div class="menu">
-      <a 
-        class="item button icon"
-        :href="googleLink"
-      >
-        <i class="google icon" />
-        Google
-      </a>
+    <div class="ui modal">
+      <div class="header">
+        Add to Calendar
+      </div>
+
+      <!-- calendar options -->
+      <div class="ui content menu vertical">
+        <a 
+          class="ui button fluid google-link"
+          :href="googleLink"
+        >
+          Google
+        </a>
+
+        <a 
+          class="ui button fluid disabled outlook-link"
+        >
+          Outlook
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -65,8 +79,11 @@ export default {
       ].join('&')
     }
   },
-  mounted: function() {
-    this.$jQuery('.ui.dropdown').dropdown()
+  methods: {
+    clickHandler: function() {
+      $('.ui.modal')
+        .modal('show')
+    },
   }
 }
 </script>
@@ -80,6 +97,16 @@ export default {
 .basic.positive {
   color: #374b39 !important;
   box-shadow: 0 0 0 1px inset !important;
+}
+
+.google-link {
+	background: url("../../../assets/search.svg") 18px 50% no-repeat;
+	background-size: 22px 100%;
+}
+
+.outlook-link {
+	background: url("../../../assets/outlook.svg") 18px 50% no-repeat;
+	background-size: 22px 100%;
 }
 
 </style>
